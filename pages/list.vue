b791097fbf80655cdb047a255b281e12e9f85503
humbertodlacerda <humbertodlacerda@gmail.com>
2022-12-14 19:28:12 -0300

    feat: :sparkles: Update

--- pages/list.vue
+++ pages/list.vue
@@ -1,51 +1,31 @@
 <template>
-
     <div>
-        <!-- <table class="m-auto mt-5 table-fixed border-spacing-2">
-            <thead>
-                <tr class="odd:bg-white even:bg-slate-50">
-                    <th class="border-2 p-1 border-slate-600" >Data</th>
-                    <th class="border-2 p-1 border-slate-600" >Descrição</th>
-                    <th class="border-2 p-1 border-slate-600" >Valor</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="expense in expenses" :key="expense" class="">
-                    <td class=" border-2 p-1 border-slate-700">{{expense.date}}</td>
-                    <td class=" border-2 p-1 border-slate-700">{{expense.description}}</td>
-                    <td class=" border-2 p-1 border-slate-700">{{expense.value}}</td>
-                </tr>
-            </tbody>
-        </table> -->
-
-
-
-        <div class="m-5 overflow-x-auto relative shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-blue-100 dark:text-blue-100">
-        <thead class="text-xs text-white uppercase bg-blue-600 dark:text-white">
-            <tr>
-                <th scope="col" class="py-3 px-6">Data</th>
-                <th scope="col" class="py-3 px-6">Descrição</th>
-                <th scope="col" class="py-3 px-6">Category</th>
-                <th scope="col" class="py-3 px-6">Valor</th>
-                <th scope="col" class="py-3 px-6">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="expense in expenses" :key="expense" class="bg-blue-500 border-b border-blue-400">
-                <td class="py-4 px-6">{{expense.date}}</td>
-                <td class="py-4 px-6">{{expense.description}}</td>
-                <td class="py-4 px-6">null</td>
-                <td class="py-4 px-6">{{expense.value}}</td>
-                <td class="py-4 px-6">
-                    <a href="#" class="font-medium text-white hover:underline">Edit</a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-
-
+        <div class="m-8 overflow-x-auto relative shadow-md sm:rounded-lg">
+            <button @click="newRevenue()" class="flex-auto float-right mr-0 m-1 bg-[#2EFE2E] rounded w-28">Receita</button>
+            <button @click="newExpense()" class="flex-auto float-right m-1 bg-[#FF0000] rounded w-28">Despesa</button>
+            <table class="w-full text-sm text-left text-blue-100 dark:text-blue-100">
+                <thead class="text-xs text-white uppercase bg-blue-600 dark:text-white">
+                    <tr>
+                        <th scope="col" class="py-3 px-6">Data</th>
+                        <th scope="col" class="py-3 px-6">Descrição</th>
+                        <th scope="col" class="py-3 px-6">Category</th>
+                        <th scope="col" class="py-3 px-6">Valor</th>
+                        <th scope="col" class="py-3 px-6">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="expense in expenses" :key="expense.id" class="bg-blue-500 border-b border-blue-400">
+                        <td type="date" class="py-4 px-6">{{expense.date}}</td>
+                        <td class="py-4 px-6">{{expense.description}}</td>
+                        <td class="py-4 px-6">null</td>
+                        <td class="py-4 px-6">{{expense.value}}</td>
+                        <td class="py-4 px-6">
+                            <a href="#" class="font-medium text-white hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -60,6 +40,18 @@ export default {
         this.$store.dispatch('expense/getExpenses').then(({ data }) => {
         this.expenses = data
       });
+    },
+    methods: {
+        newRevenue() {
+            this.$router.push({
+                path:'/revenue',
+            });
+        },
+        newExpense() {
+            this.$router.push({
+                path:'/expense',
+            });
+        }
     }
 }
 </script>
